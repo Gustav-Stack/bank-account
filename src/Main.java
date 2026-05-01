3bf0e522c2bfc800d7f1233cfb61ceffe4b27af1
Gustav-Stack <lucenagustavo404@gmail.com>
2026-05-01 13:11:15 -0300

    feat adding methods atributes and constructors

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
 
         ContaBancaria contabancaria = new ContaBancaria("Gustavo",10);
 
-        contabancaria.depositar(10);
-        contabancaria.sacar(5);
+        contabancaria.depositar(500);
+        contabancaria.sacar(7);
         contabancaria.exibirSaldo();
 
         ContaBancaria contabancaria1 = new ContaBancaria("Juan",40);
-        contabancaria1.depositar(10);
-        contabancaria1.sacar(5);
+        contabancaria1.depositar(2000);
+        contabancaria1.sacar(359);
         contabancaria1.exibirSaldo();
 
 
